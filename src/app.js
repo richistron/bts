9534d0d62a8d29e02ebd40317e3b4de44266513f
Ricardo Rivas <richistron@gmail.com>
2019-03-29 14:21:36 -0600

    removes redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 import Layout from './components/Layout';
 
 document.body.innerHTML = '<div id="app"></div>';
@@ -9,8 +7,6 @@ document.body.innerHTML = '<div id="app"></div>';
 const rootElement = document.getElementById('app');
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Layout />
-  </Provider>,
+  <Layout />,
   rootElement
 );
